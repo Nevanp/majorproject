421dfeee0cb491f5eed39045b5f9360c74d64327
Nevanp <43014130+Nevanp@users.noreply.github.com>
2018-12-14 14:12:30 -0600

    made game sketch file

--- sketch.js
+++ sketch.js
@@ -1,17 +1,11 @@
-
 var config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 200 }
-        }
-    },
     scene: {
         preload: preload,
-        create: create
+        create: create,
+        update: update
     }
 };
 
@@ -19,30 +13,13 @@ var game = new Phaser.Game(config);
 
 function preload ()
 {
-    this.load.setBaseURL('http://labs.phaser.io');
-
-    this.load.image('sky', 'assets/skies/space3.png');
-    this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-    this.load.image('red', 'assets/particles/red.png');
 }
 
 function create ()
 {
-    this.add.image(400, 300, 'sky');
+    new Text(game.scene, 300, 300, "hello world")
+}
 
-    var particles = this.add.particles('red');
-
-    var emitter = particles.createEmitter({
-        speed: 100,
-        scale: { start: 1, end: 0 },
-        blendMode: 'ADD'
-    });
-
-    var logo = this.physics.add.image(400, 100, 'logo');
-
-    logo.setVelocity(100, 200);
-    logo.setBounce(1, 1);
-    logo.setCollideWorldBounds(true);
-
-    emitter.startFollow(logo);
+function update ()
+{
 }
