24e9918d03bc0db332344fac1e51d9316b16a347
Nevanp <43014130+Nevanp@users.noreply.github.com>
2019-01-09 14:11:00 -0600

    enemy

--- sketch.js
+++ sketch.js
@@ -30,6 +30,9 @@ var keyD;
 var keyS;
 var counter = 0;
 var playerSpeed = 250;
+var enemyspeed = 50;
+var counter2 = 0;
+var state = 1;
 
 var game = new Phaser.Game(config);
 
@@ -38,15 +41,17 @@ function preload ()
     this.load.image('isaac', 'assets/isaac.png');
     this.load.image('tear', 'assets/teardrop1.png');
     this.load.image('background', 'assets/room.png');
+    this.load.image('enemy', 'assets/enemy.png');
+    this.load.image('blood', 'assets/blooddrop.png');
 }
 
 function create ()
 {
     background = this.add.image(config.width / 2, config.height / 2 ,'background');
-    background.scaleX = .55;
-    background.scaleY = 0.7;
-    x = 300;
-    y = 300;
+    background.scaleX = .6;
+    background.scaleY = 0.8;
+    x = 400;
+    y = 400;
     cursors = this.input.keyboard.createCursorKeys();
     keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
     keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
@@ -54,70 +59,148 @@ function create ()
     keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
 
     
-
+    enemy = this.physics.add.image(100, 100, 'enemy');
     player = this.physics.add.image(x, y, 'isaac');
+    
     player.displayOriginX = (10);
     player.displayOriginY = (1);
+
+    enemy.displayOriginX = (10);
+    enemy.displayOriginY = (1);
 
     player.setCollideWorldBounds(true);
     player.scaleX = 0.5;
     player.scaleY = 0.5;
+    enemy.setCollideWorldBounds(true);
+    enemy.scaleX = 0.5;
+    enemy.scaleY = 0.5;
     keySpace = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+    // this.physics.add.collider(enemy, 'tear');
+    // this.physics.add.collider(player, 'blood');
     
 }
 
 function update ()
 {
+    if(state === 1){
+        counter2 ++;
+        player.setVelocity(0);
+
+        if (cursors.left.isDown)
+        {
+            player.setVelocityX(-playerSpeed);
+            checkKey = "left";
+        }
+        else if (cursors.right.isDown)
+        {
+            player.setVelocityX(playerSpeed);
+            checkKey = "right";
+        }
+
+        if (cursors.up.isDown)
+        {
+            player.setVelocityY(-playerSpeed);
+            checkKey = "up";
+        }
+        else if (cursors.down.isDown)
+        {
+            player.setVelocityY(playerSpeed);
+            checkKey = "down";
+        }
+        if(counter % 30 === 0){
+            if(keyS.isDown){
+                tear = this.physics.add.image(player.x, player.y, 'tear');
+                tear.setVelocityY(500);
+
+            }
+            else if(keyW.isDown){
+                tear = this.physics.add.image(player.x, player.y, 'tear');
+                tear.setVelocityY(-500);
+                
+                }
+            else if(keyA.isDown){
+                tear = this.physics.add.image(player.x, player.y, 'tear');
+                tear.setVelocityX(-500);
+
+                }
+            else if(keyD.isDown){
+                tear = this.physics.add.image(player.x, player.y, 'tear');
+                tear.setVelocityX(500);
+
+                }
+            }
+            //shoot timer
+            if(keyA.isDown || keyS.isDown || keyD.isDown || keyW.isDown ){
+                counter ++;
+            }
+            else{
+                counter = 0;
+            }
+    //enemy movement
+            if (player.x < enemy.x)
+            {
+                enemy.setVelocityX(-enemyspeed);
+                checkKey = "left";
+            }
+            else if (player.x > enemy.x)
+            {
+                enemy.setVelocityX(enemyspeed);
+                checkKey = "right";
+            }
+
+            if (player.y < enemy.y)
+            {
+                enemy.setVelocityY(-enemyspeed);
+                checkKey = "up";
+            }
+            else if (player.y > enemy.y)
+            {
+                enemy.setVelocityY(enemyspeed);
+                checkKey = "down";
+            }
+            //enemy fire
+            if(counter2 % 45 === 0){
+            if(player.y < enemy.y && dist(enemy.x, player.x) < dist(enemy.y, player.y)){
+                blood = this.physics.add.image(enemy.x, enemy.y, 'blood');
+                blood.setVelocityY(-400);
+
+            }
+            if(player.x > enemy.x && dist(enemy.x, player.x) > dist(enemy.y, player.y)){
+                blood = this.physics.add.image(enemy.x, enemy.y, 'blood');
+                blood.setVelocityX(400);
+
+            }
+            if(player.x < enemy.x && dist(enemy.x, player.x) > dist(enemy.y, player.y)){
+                blood = this.physics.add.image(enemy.x, enemy.y, 'blood');
+                blood.setVelocityX(-400);
+
+            }
+            if(player.y > enemy.y && dist(enemy.x, player.x) < dist(enemy.y, player.y)){
+                blood = this.physics.add.image(enemy.x, enemy.y, 'blood');
+                blood.setVelocityY(400);
+
+            }
+            this.physics.add.collider(player, blood, gameover, null, this);
+        }
+    }
+}
+
+
+function gameover(){
+    state = 2;
+    this.add.image(config.width / 2, config.height / 2 ,'background');
     
-    player.setVelocity(0);
+    text = this.add.text(config.width/6, config.height / 2, "You Lose", { font: "74px Times New Roman", fill: "#f00" } );
+}
 
-    if (cursors.left.isDown)
-    {
-        player.setVelocityX(-playerSpeed);
-        checkKey = "left";
+
+function dist(p1, p2){
+    var ans = p1 - p2;
+    if(ans < 0){
+        ans = ans * -1;
+        return ans;
     }
-    else if (cursors.right.isDown)
-    {
-        player.setVelocityX(playerSpeed);
-        checkKey = "right";
+    else{
+        return ans;
     }
-
-    if (cursors.up.isDown)
-    {
-        player.setVelocityY(-playerSpeed);
-        checkKey = "up";
-    }
-    else if (cursors.down.isDown)
-    {
-        player.setVelocityY(playerSpeed);
-        checkKey = "down";
-    }
-    if(counter % 30 === 0){
-        if(keyS.isDown){
-            tear = this.physics.add.image(player.x, player.y, 'tear');
-            tear.setVelocityY(500);
-
-        }
-        else if(keyW.isDown){
-            tear = this.physics.add.image(player.x, player.y, 'tear');
-            tear.setVelocityY(-500);
-            
-            }
-        else if(keyA.isDown){
-            tear = this.physics.add.image(player.x, player.y, 'tear');
-            tear.setVelocityX(-500);
-
-            }
-        else if(keyD.isDown){
-            tear = this.physics.add.image(player.x, player.y, 'tear');
-            tear.setVelocityX(500);
-
-            }
-        }
-        if(keyA.isDown || keyS.isDown || keyD.isDown || keyW.isDown ){
-            counter ++;
-        }
-        else{
-            counter = 0;
-        }
 }
