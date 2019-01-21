5ae0c8d4dd58fec6224ba0f88912935a2771293e
BuildTools <unconfigured@null.spigotmc.org>
2019-01-21 00:40:14 -0600

    rooms and flys

--- sketch.js
+++ sketch.js
@@ -36,6 +36,11 @@ var state = 1;
 var enemyHealth = 3;
 var playerHealth = 10;
 var heart = [];
+var room1;
+var room2;
+var fly;
+var flySpeed = 300;
+var flyHealth = 2;
 
 
 var game = new Phaser.Game(config);
@@ -45,15 +50,23 @@ function preload ()
     this.load.image('isaac', 'assets/isaac.png');
     this.load.image('tear', 'assets/teardrop1.png');
     this.load.image('background', 'assets/room.png');
+    this.load.image('room2', 'assets/room3.png');
+    this.load.image('room3', 'assets/room2.png');
     this.load.image('enemy', 'assets/enemy.png');
     this.load.image('blood', 'assets/blooddrop.png');
     this.load.image('heart', 'assets/heart.png');
+    this.load.image('fly', 'assets/fly.png');
 }
 
 function create ()
 {
     blood = this.physics.add.image(0, 0, 'blood');
     tear = this.physics.add.image(0, 0, 'tear');
+    fly = this.physics.add.image(100, 100, 'fly');
+    enemy = this.physics.add.image(100, 100, 'enemy');
+    room2 = this.add.image(config.width / 2, config.height / 2 ,'room2');
+    room1 = this.add.image(config.width / 2, config.height / 2 ,'background');
+    room3 = this.add.image(config.width / 2, config.height / 2 ,'room3');
     background = this.add.image(config.width / 2, config.height / 2 ,'background');
     background.scaleX = .6;
     background.scaleY = 0.8;
@@ -66,7 +79,7 @@ function create ()
     keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
 
     
-    enemy = this.physics.add.image(100, 100, 'enemy');
+    
     player = this.physics.add.image(x, y, 'isaac');
     
     // player.displayOriginX = (10);
@@ -78,24 +91,22 @@ function create ()
     player.setCollideWorldBounds(true);
     player.scaleX = 0.5;
     player.scaleY = 0.5;
-    enemy.setCollideWorldBounds(true);
-    enemy.scaleX = 0.5;
-    enemy.scaleY = 0.5;
+
     keySpace = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
     // this.physics.add.collider(enemy, 'tear');
-    this.physics.add.collider(player, enemy);
+    
     for(let i = playerHealth; i > 0; i --){
         heart[i] = this.add.image(20*i, 20, 'heart');
         heart[i].scaleX = 0.5;
         heart[i].scaleY = 0.5;
     }
-    
+
     
 }
 
 function update ()
 {
-    if(state === 1){
+    if(state !== 2){
         counter2 ++;
         player.setVelocity(0);
 
@@ -150,62 +161,149 @@ function update ()
                 }
                 counter = 0;
             }
-            //shoot timer
-            
-            
-            if(enemyHealth > 0){
-    //enemy movement
-                if (player.x < enemy.x)
-                {
-                    enemy.setVelocityX(-enemyspeed);
-                    checkKey = "left";
+            //state finder/ room finder
+            if(player.x === 37 && player.y > 275 && player.y < 325 && state !== 3){
+                player.x = 760;
+                if(state === 1){
+                    state = 3;
+                    background = this.add.image(config.width / 2, config.height / 2 ,'room2');
+                    enemy = this.physics.add.image(100, 100, 'enemy');
+                    enemy.setCollideWorldBounds(true);
+                    enemy.scaleX = 0.5;
+                    enemy.scaleY = 0.5;
+                    this.physics.add.collider(player, enemy);
+                    background.scaleX = .6;
+                    background.scaleY = 0.8;
+                    player = this.physics.add.image(player.x, player.y, 'isaac');
+                    player.setCollideWorldBounds(true);
+                    player.scaleX = 0.5;
+                    player.scaleY = 0.5;
                 }
-                else if (player.x > enemy.x)
-                {
-                    enemy.setVelocityX(enemyspeed);
-                    checkKey = "right";
+                else if(state === 4){
+                    state = 1;
+                    background = this.add.image(config.width / 2, config.height / 2 ,'background');;
+                    background.scaleX = .6;
+                    background.scaleY = 0.8;
+                    player = this.physics.add.image(player.x, player.y, 'isaac');
+                    player.setCollideWorldBounds(true);
+                    player.scaleX = 0.5;
+                    player.scaleY = 0.5;
                 }
+            }
+            if(player.x > 763 && player.y > 275 && player.y < 325 && state !== 4){
+                player.x = 40;
+                if(state === 3){
+                    state = 1;
+                    background = this.add.image(config.width / 2, config.height / 2 ,'background');
+                    background.scaleX = .6;
+                    background.scaleY = 0.8;
+                    player = this.physics.add.image(player.x, player.y, 'isaac');
+                    player.setCollideWorldBounds(true);
+                    player.scaleX = 0.5;
+                    player.scaleY = 0.5;
+                }
+                else if (state === 1){
+                    state = 4;
+                    background = this.add.image(config.width / 2, config.height / 2 ,'room3');
+                    room3;
+                    background.scaleX = .6;
+                    background.scaleY = 0.8;
+                    player = this.physics.add.image(player.x, player.y, 'isaac');
+                    player.setCollideWorldBounds(true);
+                    player.scaleX = 0.5;
+                    player.scaleY = 0.5;
+                    
+                }
+            }
+            if(state === 3){
+            
+                if(enemyHealth > 0){
+        //enemy movement
+                    if (player.x < enemy.x)
+                    {
+                        enemy.setVelocityX(-enemyspeed);
+                        
+                    }
+                    else if (player.x > enemy.x)
+                    {
+                        enemy.setVelocityX(enemyspeed);
+                       
+                    }
 
-                if (player.y < enemy.y)
-                {
-                    enemy.setVelocityY(-enemyspeed);
-                    checkKey = "up";
-                }
-                else if (player.y > enemy.y)
-                {
-                    enemy.setVelocityY(enemyspeed);
-                    checkKey = "down";
-                }
-                //enemy fire
+                    if (player.y < enemy.y)
+                    {
+                        enemy.setVelocityY(-enemyspeed);
+                        
+                    }
+                    else if (player.y > enemy.y)
+                    {
+                        enemy.setVelocityY(enemyspeed);
+                     
+                    }
+                    //enemy fire
+                    
+                    if(counter2 % 45 === 0){
+                        bloodRemover();
+                    if(player.y < enemy.y && dist(enemy.x, player.x) < dist(enemy.y, player.y)){
+                        blood = this.physics.add.image(enemy.x, enemy.y, 'blood');
+                        blood.setVelocityY(-400);
+                        this.physics.add.collider(blood, player, gameover, bloodRemover, this);
+                    }
+                    if(player.x > enemy.x && dist(enemy.x, player.x) > dist(enemy.y, player.y)){
+                        blood = this.physics.add.image(enemy.x, enemy.y, 'blood');
+                        blood.setVelocityX(400);
+                        this.physics.add.collider(blood, player, gameover, bloodRemover, this);
+                    }
+                    if(player.x < enemy.x && dist(enemy.x, player.x) > dist(enemy.y, player.y)){
+                        blood = this.physics.add.image(enemy.x, enemy.y, 'blood');
+                        blood.setVelocityX(-400);
+                        this.physics.add.collider(blood, player, gameover, bloodRemover, this);
+                    }
+                    if(player.y > enemy.y && dist(enemy.x, player.x) < dist(enemy.y, player.y)){
+                        blood = this.physics.add.image(enemy.x, enemy.y, 'blood');
+                        blood.setVelocityY(400);
+                        this.physics.add.collider(blood, player, gameover, bloodRemover, this);
+                    }
                 
-                if(counter2 % 45 === 0){
-                    bloodRemover();
-                if(player.y < enemy.y && dist(enemy.x, player.x) < dist(enemy.y, player.y)){
-                    blood = this.physics.add.image(enemy.x, enemy.y, 'blood');
-                    blood.setVelocityY(-400);
-                    this.physics.add.collider(blood, player, gameover, bloodRemover, this);
+                
                 }
-                if(player.x > enemy.x && dist(enemy.x, player.x) > dist(enemy.y, player.y)){
-                    blood = this.physics.add.image(enemy.x, enemy.y, 'blood');
-                    blood.setVelocityX(400);
-                    this.physics.add.collider(blood, player, gameover, bloodRemover, this);
-                }
-                if(player.x < enemy.x && dist(enemy.x, player.x) > dist(enemy.y, player.y)){
-                    blood = this.physics.add.image(enemy.x, enemy.y, 'blood');
-                    blood.setVelocityX(-400);
-                    this.physics.add.collider(blood, player, gameover, bloodRemover, this);
-                }
-                if(player.y > enemy.y && dist(enemy.x, player.x) < dist(enemy.y, player.y)){
-                    blood = this.physics.add.image(enemy.x, enemy.y, 'blood');
-                    blood.setVelocityY(400);
-                    this.physics.add.collider(blood, player, gameover, bloodRemover, this);
-                }
-               
-              
             }
         }
+            //fly moving
+            if(state === 4){
+                
+                    fly = this.physics.add.image(400, 100, 'fly');
+                    fly.setCollideWorldBounds(true);
+                    fly.scaleX = 0.5;
+                    fly.scaleY = 0.5;
+                    this.physics.add.collider(tear, fly, ouch);
+                    this.physics.add.collider(player, fly, gameover, null, this);
+                    //fly move
+                    if (player.x < fly.x)
+                    {
+                        fly.setVelocityX(-flySpeed);
+                        
+                    }
+                    else if (player.x > fly.x)
+                    {
+                        fly.setVelocityX(flySpeed);
+                       
+                    }
+
+                    if (player.y < fly.y)
+                    {
+                        fly.setVelocityY(-flySpeed);
+                        
+                    }
+                    else if (player.y > fly.y)
+                    {
+                        fly.setVelocityY(flySpeed);
+                     
+                    }
+            }
     }
 }
+
 
 
 function gameover(){
@@ -237,9 +335,18 @@ function dist(p1, p2){
 
 function ouch(){
     tear.disableBody(true, true);
-    enemyHealth --;
-    if(enemyHealth <= 0){
-        enemy.disableBody(true, true);
+    if(state === 3){
+        enemyHealth --;
+        if(enemyHealth <= 0){
+            enemy.disableBody(true, true);
+        }
+    }
+    if(state === 4){
+        flyHealth --;
+        if(flyHealth <= 0){
+
+            fly.disableBody(true, true);
+        }
     }
 }
 
